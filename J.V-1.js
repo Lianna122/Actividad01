9c4d39b1b724ec31a25d413d50d23794ea7df4d1
Lianna122 <71226137@idexpasco.edu.pe>
2025-04-28 14:52:37 -0500

    JAVASCRIP

---
+++ J.V-1.js
@@ -0,0 +1,8 @@
+
+ const boton = document.getElementById("botonMensaje");
+  
+ const parrrafo = document.querySelector(".cambiar estilo");
+
+ boton.addEventListener("onclick", function() {
+  parrafo.textContent = "¡El estilo ha cambiado con JavaScript!😄";
+});
